c020f04919ead8ef168d81f944219eacb70bbf0b
Stijn Jonckheere <jonckheere_stijn@hotmail.com>
2018-09-30 18:13:34 +0200

    added basic operations

--- intro-web-basics/scripts/basic-variables.js
+++ intro-web-basics/scripts/basic-variables.js
@@ -9,3 +9,32 @@ var isMale = true;
 print('This is text', name);
 print('This is a number', age);
 print('This is a boolean', isMale);
+
+// Basic operations
+
+name = "Sir " + name + " PhD";
+
+print('This is concatenated text', name);
+
+age = age + 5;
+
+print("Number increased", age);
+
+var stringNumber = name + age;
+
+print("Adding a number to a string", stringNumber);
+
+var ageBoolean = isMale + age;
+
+print("Adding true to a number", ageBoolean);
+
+ageBoolean = false + age;
+
+print("Adding false to a number", ageBoolean);
+
+// Experiment
+
+var someText = "Some text";
+var result = someText / 10;
+
+print("Dividing a string by 10", result);
